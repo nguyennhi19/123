7816d6ed07c0aba7a6d156f69796ce633283c539
nguyennhi19 <nhinguyen1912200024@gmail.com>
2022-11-08 16:58:57 +0700

    update file BookTicketPage

--- src/main/java/PageObjects/BookTicketPage.java
+++ src/main/java/PageObjects/BookTicketPage.java
@@ -2,26 +2,36 @@ package PageObjects;
 
 import Common.Constant.Constant;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
-public class BookTicketPage {
-    //Locator12345
+import java.util.concurrent.TimeUnit;
 
-    private final By loc_selectDate = By.xpath("//select[@name='Date']");
-    private final By loc_selectDepartFrom = By.xpath("//select[@name='DepartStation']");
-    private final By loc_selectArriverAt = By.xpath("//select[@name='ArriveStation']");
-    private final By loc_selectSeatType = By.xpath("//select[@name='SeatType']");
-    private final By loc_selectTicketAmount = By.xpath("//select[@name='TicketAmount']");
+public class BookTicketPage {
+    //Locator
+
+    private final By loc_successfullyTitle = By.xpath("//*[@id='content']/h1");
+    private final By loc_selectDate = By.xpath("//select[@name='Date']//option");
+    private final By loc_selectDepartFrom = By.xpath("//select[@name='DepartStation']//option");
+    private final By loc_selectArriverAt = By.xpath("//select[@name='ArriveStation']//option");
+    private final By loc_selectSeatType = By.xpath("//select[@name='SeatType']//option");
+    private final By loc_selectTicketAmount = By.xpath("//select[@name='TicketAmount']//option");
     private final By loc_btnLBookTicket = By.xpath("//input[@value='Book ticket']");
+    private final By loc_errorMessageAtArriveStation = By.xpath("//label[text()= 'Arrive at:']/following-sibling::label");
+    private final By loc_errorMessageAtTicketAmount = By.xpath("//label[text()= 'Ticket amount:']/following-sibling::label");
+
+
 
     //Elements
 
     private WebElement getSelectDate(){
         return Constant.driver.findElement(loc_selectDate);
     }
+
     private WebElement getSelectDepartFrom(){
         return Constant.driver.findElement(loc_selectDepartFrom);
     }
+
     private WebElement getSelectArriverAt(){
         return Constant.driver.findElement(loc_selectArriverAt);
     }
@@ -29,16 +39,37 @@ public class BookTicketPage {
     private WebElement getSelectSeatType(){
         return Constant.driver.findElement(loc_selectSeatType);
     }
+
     private WebElement getSelectTicketAmount(){
         return Constant.driver.findElement(loc_selectTicketAmount);
+    }
+
+    private void getScrollToElement(){
+        WebElement element = Constant.driver.findElement(loc_btnLBookTicket);
+        ((JavascriptExecutor) Constant.driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        Constant.driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
     }
 
     private WebElement getBtnBookTicket(){
         return Constant.driver.findElement(loc_btnLBookTicket);
     }
 
+
+    private WebElement getSuccessfulTitle(){
+        return Constant.driver.findElement(loc_successfullyTitle);
+    }
+
+    private WebElement getErrorMessageAtArriveStation(){
+        return Constant.driver.findElement(loc_errorMessageAtArriveStation);
+    }
+
+    private WebElement getErrorMessageAtTicketAmount(){
+        return Constant.driver.findElement(loc_errorMessageAtTicketAmount);
+    }
+
     //Methods
     public void chooseDate(String date) {
+
         this.getSelectDate().click();
         Constant.driver.findElement(By.xpath("//select[@name='Date']//option[text()='"+date+"']")).click();
     }
@@ -46,25 +77,26 @@ public class BookTicketPage {
 
     public void chooseDepartFrom(String departFrom) {
         this.getSelectDepartFrom().click();
-        Constant.driver.findElement(By.xpath("//select[@name='Date']//option[text()='"+departFrom+"']")).click();
+        Constant.driver.findElement(By.xpath("//select[@name='DepartStation']//option[text()='"+departFrom+"']")).click();
     }
     //select[@name='Date']//option[text()='Huế']
 
     public void chooseArriveAt(String arriveAt) {
         this.getSelectArriverAt().click();
-        Constant.driver.findElement(By.xpath("//select[@name='Date']//option[text()='"+arriveAt+"']")).click();
+        Constant.driver.manage().timeouts().implicitlyWait(4000, TimeUnit.SECONDS);
+        Constant.driver.findElement(By.xpath("//select[@name='ArriveStation']//option[text()='"+arriveAt+"']")).click();
     }
     //select[@name='Date']//option[text()='Sài Gòn']
 
     public void chooseSeatType(String seatType) {
         this.getSelectSeatType().click();
-        Constant.driver.findElement(By.xpath("//select[@name='Date']//option[text()='"+seatType+"']")).click();
+        Constant.driver.findElement(By.xpath("//select[@name='SeatType']//option[text()='"+seatType+"']")).click();
     }
     //select[@name='Date']//option[text()='Hart seat']
 
     public void chooseTicketAmount(String ticketAmount) {
         this.getSelectTicketAmount().click();
-        Constant.driver.findElement(By.xpath("//select[@name='Date']//option[text()='"+ticketAmount+"']")).click();
+        Constant.driver.findElement(By.xpath("//select[@name='TicketAmount']//option[text()='"+ticketAmount+"']")).click();
     }
     //select[@name='Date']//option[text()='1']
 
@@ -74,6 +106,18 @@ public class BookTicketPage {
         this.chooseArriveAt(arriverAt);
         this.chooseSeatType(seatType);
         this.chooseTicketAmount(ticketAmount);
+        this.getScrollToElement();
         this.getBtnBookTicket().click();
+    }
+
+    public String NavigateSuccessfulScreen(){
+        return this.getSuccessfulTitle().getText();
+    }
+
+    public String VerifyErrorMessageAtArriveStation(){
+        return this.getErrorMessageAtArriveStation().getText();
+    }
+    public String VerifyErrorMessageAtTicketAmount(){
+        return this.getErrorMessageAtTicketAmount().getText();
     }
 }
